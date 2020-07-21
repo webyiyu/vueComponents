5097361d6ed92aa5b5d1ccf2a09f9450ebe4b2cc
haiyan <qihaiyan@hjzddata.com>
2020-07-21 15:11:16 +0800

    增加全局组件 alert

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Alert from "../src/components/alert/alert.js";
+
 Vue.use(router);
 
 Vue.config.productionTip = false
+Vue.prototype.$Alert = Alert
 
 new Vue({
-  render: h=> h(App),
+  render: h => h(App),
   router
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Router from 'vue-router'
 import CheckBox from '@/pages/checkbox';
 import HelloWorld from "@/pages/helloworld";
 import Display from "@/pages/display";
+import Alert from "@/pages/alert"
 
 Vue.use(Router);
 
@@ -23,6 +24,11 @@ export default new Router({
             path: '/display',
             name: 'Display',
             component: Display
+        },
+        {
+            path: '/alert',
+            name: 'Alert',
+            component: Alert
         },
 
     ]
